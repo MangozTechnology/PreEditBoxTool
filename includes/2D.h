34280f10f3c569fd76eb2e41677b7c105988e6c5
MangozTechnology <mangoztechnology@gmail.com>
2025-12-06 13:30:15 -0600

    Update 2D.h

--- includes/2D.h
+++ includes/2D.h
@@ -35,6 +35,7 @@ public:
 
    int 2D_size;
 
+   //grid type
    enum : int {
      engineer_grid = 0x0,
      editor_grid = 0x1
@@ -43,10 +44,46 @@ public:
    virtual void Paint() = 0;
    virtual void PrintDimensions() = 0;
 
+   //for changing background event
    virtual void OverrideBackground() = 0;
 
-   unsigned char RGBAPixelBuff;
-   
+   //background color
+   unsigned char RGBAPixelChannel;
+
+   virtual void ConnectLast2D(BoxTool2D& prev2d) = 0;
+
+   //picture
+   unsigned char** pic;
+
+   const char* Label;
+
+   char xyBuff[2048];
+
+   virtual void DoPenColor(BoxTool2D& box, unsigned char& COLOR)= 0;
+   virtual void Do3DBox() = 0;
+   virtual void DoBrushCreation() = 0;
+   virtual void DoDynamicCreation() = 0;
+   virtual void DoStaticCreation() = 0;
+   virtual void DoBrushFaceCreation() = 0;
+   virtual void DoPlaneCreation() = 0;
+   virtual void DoWindingCreation() = 0;
+   virtual void DoSelection() = 0;
+
+   virtual void SelectBrush() = 0;
+   virtual void SelectFace() = 0;
+   virtual void SelectVertex() = 0;
+   virtual void SelectBox() = 0;
+
+   //enables drawing, selecting, dragging and zooming
+   bool bXYGlobalMode;
+
+   virtual void Update() = 0;
+   virtual void QueueDraw() = 0;
+   virtual void QueueXY() = 0;
+   virtual void QueueXZ() = 0;
+   virtual void QueueYZ() = 0;
+
+
 };
 
 #endif
