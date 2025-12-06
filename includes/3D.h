46cf36afd43bcdb1cabac34a2d89fc08d4cf8269
MangozTechnology <mangoztechnology@gmail.com>
2025-12-06 15:23:09 -0600

    Create 3D.h

---
+++ includes/3D.h
@@ -0,0 +1,59 @@
+/*3D.h*/
+/*PreEditBoxTool copyright(c)*/
+
+#ifndef _3D_H_
+#define _3D_H_
+
+#include "2D.h"
+
+/*
+  3D Camera Implementation
+*/
+class 3DBoxTool : public 2DBoxTool {
+public:
+   3DBoxTool();
+   virtual ~3DBoxTool();
+
+   bool bIsGL;
+   bool IsD3D;
+
+   int x_pos;
+   int y_pos;
+   int width;
+   int height;
+
+   unsigned char RGBACameraColor;
+
+   int lightmode;
+
+   bool bFreeze;
+
+   vec3_t origin;
+   vec3_t angles;
+   vec3_t* prev_angles;
+
+   float near;
+   float far;
+
+   float zBuffer;
+
+   int m_nBrushCount;
+   int m_nEntityCount;
+   int m_nPlaneCount;
+   int m_nFaceCount;
+
+   virtual void senderConnectLast() = 0;
+
+   virtual void ShowBrush() = 0;
+   virtual void ShowEntity() = 0;
+   virtual void ShowWorkzone() = 0;
+
+   virtual void CameraSelectBrush() = 0;
+   virtual void CamersSelectSide() = 0;
+
+   virtual void AddToSceneGraph() = 0;
+
+
+};
+
+#endif
